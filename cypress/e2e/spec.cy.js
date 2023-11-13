4d06d0de1d5d10242dac0231466f204927a91c19
Vesa Laakso <482561+valscion@users.noreply.github.com>
2023-11-13 11:21:29 +0200

    Test workaround: Clearing cookies before fetch()

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,6 +6,12 @@ describe('issue https://github.com/cypress-io/cypress/issues/25841 reproduction'
     cy.contains('FLASH_FROM_REDIRECT_ACTION').should('be.visible')
     cy.visit('/page_b')
     cy.contains('THIS_SHOULD_BE_VISIBLE_AFTER_BUTTON_CLICK').should('not.exist')
+
+    // Workaround: Clearing the cookies before triggering the fetch request
+    // Inspired by the workaround posted by @ksazhnev here:
+    // https://github.com/cypress-io/cypress/issues/25841#issuecomment-1807468630
+    cy.clearCookies()
+
     cy.contains('Trigger fetch request').click()
     cy.contains('THIS_SHOULD_BE_VISIBLE_AFTER_BUTTON_CLICK').should('be.visible')
   })
